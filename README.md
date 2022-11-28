552b3e43071fdbc169ceca0fde0f2e1c6f2b74ef
luca cadamuro <cadamuro@atlas1.ijclab.in2p3.fr>
2022-11-28 09:37:43 +0100

    mHH split code

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Tools for EFT interpretation in the ATLAS HH->bbgg analysis
 2. Install quickstats : https://gitlab.cern.ch/clcheng/quickstats/-/tree/master/
 3. Download the input workspaces from the bbgg analysis: https://gitlab.cern.ch/atlas-physics/HDBS/DiHiggs/yybb/database/-/tree/master
 4. Download this repository
+
+## Modification of a workspace to include EFT effects - ``workspace_scripts``
+
+You first need to determine the yield for each mHH bin. This is done with the script ``make_mHH_split.py`` that generates a file containing the information split by mHH bin.
+
+The input ntuples can be downloaded from : ``/eos/user/c/chlcheng/analysis/bbyy/VBF_optimization/preselected_h027_01_04_2022/outputs/h027_final_legacy/minitrees``.
---
+++ workspace_scripts/make_mHH_split.py
@@ -0,0 +1,112 @@
+import ROOT
+import pickle
+
+categs = [
+    'SM_1',
+    'SM_2',
+    'SM_3',
+    'BSM_1',
+    'BSM_2',
+    'BSM_3',
+    'BSM_4',
+]
+
+# files = {
+#     'SM_1' : 'inputNtuples/HH_SM_1.root',
+#     'SM_2' : 'inputNtuples/HH_SM_2.root',
+#     'SM_3' : 'inputNtuples/HH_SM_3.root',
+#     'BSM_1' : 'inputNtuples/HH_BSM_1.root',
+#     'BSM_2' : 'inputNtuples/HH_BSM_2.root',
+#     'BSM_3' : 'inputNtuples/HH_BSM_3.root',
+#     'BSM_4' : 'inputNtuples/HH_BSM_4.root',
+# }
+
+files = {
+    'SM_1' : '../../inputNtuples/HHbbyy_cHHH01d0_SM_1.root',
+    'SM_2' : '../../inputNtuples/HHbbyy_cHHH01d0_SM_2.root',
+    'SM_3' : '../../inputNtuples/HHbbyy_cHHH01d0_SM_3.root',
+    'BSM_1' : '../../inputNtuples/HHbbyy_cHHH01d0_BSM_1.root',
+    'BSM_2' : '../../inputNtuples/HHbbyy_cHHH01d0_BSM_2.root',
+    'BSM_3' : '../../inputNtuples/HHbbyy_cHHH01d0_BSM_3.root',
+    'BSM_4' : '../../inputNtuples/HHbbyy_cHHH01d0_BSM_4.root',
+}
+foutname = 'fractions_SM_for_EFT.txt'
+dboutname = 'fractions_SM_for_EFT.pkl'
+
+treename = 'output'
+mhh_branch_name = 'truth_m_hh'
+weight_branch_name = 'total_weight'
+
+# root [9] hw->Integral()
+# (double) 0.26072346
+# root [10] htw->Integral()
+# (double) 0.25511009
+
+mHH_bins = [
+    250., 270.,290.,310.,330.,350.,370.,390.,
+    410.,430.,450.,470.,490.,
+    510.,530.,550.,570.,590.,
+    610.,630.,650.,670.,690.,
+    710.,730.,750.,770.,790.,
+    810.,830.,850.,870.,890.,
+    910.,930.,950.,970.,990.,
+    1010.,1030.
+]
+
+output_data = {}
+evt_counts = {}
+
+for c in categs:
+    print ('... reading categ', c, 'from file', files[c])
+    counts = {}
+    slices = {}
+    # fIn = ROOT.TFile.Open(files[c])
+    # tIn = fIn.Get(treename)
+    df = ROOT.RDataFrame (treename, files[c], [mhh_branch_name, weight_branch_name])
+    for ib, b in enumerate(mHH_bins): # intermediate bin: b < mHH < b+1
+        if b != mHH_bins[-1]:
+            low_bound = 1000.*mHH_bins[ib]
+            high_bound = 1000.*mHH_bins[ib+1] # since values in the ntuples are in MeV
+            cond = f'{mhh_branch_name} >= {low_bound} && {mhh_branch_name} < {high_bound}'
+        else: # last bin
+            low_bound = 1000.*mHH_bins[ib]
+            cond = f'{mhh_branch_name} >= {low_bound}'
+        # print(cond)
+        counts[b] = df.Filter(cond).Count() # booked op
+        slices[b] = df.Filter(cond).Sum(weight_branch_name) # booked op
+        # output_data[c][b] = y
+    counts['total'] = df.Count()
+    slices['total'] = df.Sum(weight_branch_name) 
+
+    output_data[c] = {}
+    evt_counts[c] = {}
+    for b in mHH_bins:
+        output_data[c][b] = slices[b].GetValue()
+        evt_counts[c][b] = counts[b].GetValue()
+    output_data[c]['total'] = slices['total'].GetValue()
+    evt_counts[c]['total'] = counts['total'].GetValue()
+
+# print values
+print('... txt file with yields created as', foutname)
+fout = open(foutname, 'w')
+for c in categs:
+    tot = 0
+    tot_count = 0
+
+    print('... CATEGORY : ', c)
+    fout.write('... CATEGORY : ' + c + '\n')
+    for b in mHH_bins:
+        print ('{} : {} (nEv = {})'.format(b, output_data[c][b], evt_counts[c][b]))
+        tot += output_data[c][b]
+        tot_count += evt_counts[c][b]
+        fout.write('{} : {} (nEv = {})'.format(b, output_data[c][b], evt_counts[c][b]) + '\n')
+    print('---> TOTAL  : ', tot)
+    # print('---> TOT xc : ', output_data[c]['total'])
+    fout.write('---> TOTAL  : ' + str(tot) + '(nEv = {})'.format(tot_count) + '\n')
+    fout.write('---> TOT xc : ' + str(output_data[c]['total']) + '(nEv = {})'.format(evt_counts[c]['total']) + '\n')
+
+print('... dumping data to', dboutname)
+saved_data = {'yields' : output_data, 'mHH_bins' : mHH_bins, 'counts' : evt_counts}
+pickle.dump( saved_data, open( dboutname, "wb" ) )
+
+
